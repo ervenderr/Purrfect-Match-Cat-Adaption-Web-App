498f0818e2ef4598397011b962d12edeeb283bdb
ervenderr <ervenidjad12@gmail.com>
2024-06-25 07:32:41 +0800

    setting up client

---
+++ src/sampleData/Cats.jsx
@@ -0,0 +1,13 @@
+
+export const Cats = [
+    {
+        "catId": "1",
+        "name": "Whiskers",
+        "age": "2 years",
+        "breed": "Siamese",
+        "description": "A playful and friendly cat who loves attention.",
+        "healthStatus": "Healthy",
+        "imageUrl": "https://example.com/images/whiskers.jpg"
+        }
+    
+]          
